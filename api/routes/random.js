ec743a94c8d2813bd5cddaa60095307878154b3f
amedeekirk <amedeekirk@yahoo.com>
2019-02-11 17:34:51 -0800

    fixes to requested random :dog2:

--- api/routes/random.js
+++ api/routes/random.js
@@ -1,27 +1,30 @@
 const router = require('express').Router();
 const {selectRandom} = require('../utils/postgres');
 
-// Handles requests for random images, up to 10
+// Handles requests for random images
 router.get('/', (req,res) => {
     (async () => {
+        // Query DB for 'count' amount of dogs, up to 10
+        let requested = isNaN(req.query.count) ? 1 : req.query.count;
+        let count = !requested ? 1
+                    : requested  > 10 ? 10
+                    : requested;
 
-        let count = req.query.count || 1;
-
-        if (count > 10) {
-            res.status(400).json({
-                error: "You cannot request that many images"
-            });
-        }
-
-        // Query DB for 'count' amount of dogs
         let result = await selectRandom(count);
 
         if(result==="error") {
             res.status(500).json(result);
         }
         else if(result.length < count) {
-            res.status(400).json({
-                error: "we don't have enough doggos :("
+            res.status(206).json({
+                success: result,
+                warning: "Not enough images to fulfill request"
+            });
+        }
+        else if(count < requested) {
+            res.status(206).json({
+                success: result,
+                warning: "Maximum request size is 10"
             });
         }
         else {
